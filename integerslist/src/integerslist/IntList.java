925ec6ef9de293ac23f57a37f28d6a5d18921999
Liselotte Baeyens <liselotte.baeyens@student.kuleuven.be>
2026-03-12 20:38:15 +0100

    Implementatie met gelinkte lijst van knopen (check want niet ok)

--- integerslist/src/integerslist/IntList.java
+++ integerslist/src/integerslist/IntList.java
@@ -12,26 +12,71 @@ import java.util.Arrays;
  */
 public class IntList {
 	
+	private class Knoop {
+		int element;
+		Knoop volgende; 
+		Knoop(int element, Knoop volgende){
+			this.element = element; 
+			this.volgende = volgende;
+		}
+	}
+	/*
+	 * Kijkt na dat het aantal knopen in de gelinkte lijstj precies
+	 * gelijk is aan 'lengte( en geeft de knopen terug in een array. 
+	 * Deze methode moet je niet zelf kunnen toepassen. 
+	 */
+	private Knoop[] getKnopen() {
+		Knoop[] result = new Knoop[lengte];
+		Knoop k = eerste;
+		for (int i = 0; i < lengte; i ++) {
+			if (k == null)
+				return null;
+			result[i] = k;
+			k = k.volgende;
+		}
+		if (k != null)
+			return null;
+		return result;
+	}
+	private int lengte;
+	/**
+	 * @invar | 0<= lengte 
+	 * @invar | getKnopen()!=null
+	 * @representationObject | getKnopen()
+	 */
+	private Knoop eerste;
+	
 	/**
 	 * @post | result != null
 	 * @creates | result 
 	 */
 	public int[] getElementen(){
-		throw new RuntimeException("Not Yet Implemented");
+		int[] result = new int[lengte];
+		Knoop k = eerste;
+		for (int i = 0; i < lengte; i ++) {
+			result[i] = k.element;
+			k = k.volgende;
+		}
+		return result;
 	}
 	
 	/**
 	 * post | result == getElementen().length
 	 */
 	public int getLengte(){
-		throw new RuntimeException("Not Yet Implemented");
+		return lengte;
 	}
 	/**
 	 * @pre | 0 <= index && index < getLengte()
 	 * @post | result == getElementen()[index]
 	 */
 	public int getElement(int index) {
-		throw new RuntimeException("Not Yet Implemented");
+		Knoop k = eerste;
+		while (index > 0) {
+			index--;
+			k = k.volgende;
+		}
+		return k.element;
 	}
 	/**
 	 * @post | getLengte() ==0
@@ -47,7 +92,15 @@ public class IntList {
 	 * @post | getElement(old(getLengte()))==element 
 	 */
 	public void add(int element) {
-		throw new RuntimeException("Not Yet Implemented");
+		if (lengte==0)
+			eerste = new Knoop(element, null);
+		else {
+			Knoop k = eerste;
+			while (k.volgende != null)
+				k = k.volgende;
+			k.volgende = new Knoop(element,null);
+		}
+		lengte++;
 	}
 	
 	/**
@@ -57,7 +110,15 @@ public class IntList {
 	 * @post| Arrays.equals(getElementen(),0, getLengte(),old(getElementen()),0, getLengte())
 	 */
 	public void removeLast() {
-		throw new RuntimeException("Not Yet Implemented"); 
+		if (lengte == 1)
+			eerste = null;
+		else {
+			Knoop k = eerste;
+			while (k.volgende.volgende != null)
+				k = k.volgende;
+			k.volgende = null;
+		}
+		lengte--;
 	}
 	
 }
